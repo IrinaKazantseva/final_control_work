701a901610bf8557d0aeaee0c624a2eae3a6e698
Irina <skorpya@yandex.ru>
2022-09-04 22:01:47 +0300

    some fix

--- TaskNewMassive/Program.cs
+++ TaskNewMassive/Program.cs
@@ -5,7 +5,7 @@ void FillArray(string[] array)
     for (int i = 0; i < array.Length; i++)
     {
         Console.Write("Введите новый элемент массива: ");
-        string? A = Console.ReadLine();
+        string A = Console.ReadLine();
         array[i] = A;
     }
 }
@@ -21,51 +21,61 @@ void PrintArray(string[] array)
     Console.Write(array[array.Length - 1] + "]");
 }
 
-// Ввод первоначального массива с клавиатуры
-
-Console.WriteLine("Введите количество элементов массива: ");
-int N = Convert.ToInt32(Console.ReadLine());
-string[] array1 = new string[N];
-FillArray(array1);
-
 // Первоначальный массив задан заранее
 
 //string[] array1 = { "Hello", "2", "world", ":-)" };
 //string[] array1 = { "1234", "1567", "-2", "computer science" };
 //string[] array1 = { "Russia", "Denmark", "Kazan" };
 
-// Вычисление длины нового массива
+// Ввод первоначального массива с клавиатуры
 
-int NewArrayLength = 0;
-for (int i = 0; i < array1.Length; i++)
+Console.WriteLine("Введите количество элементов массива: ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N > 0)
 {
-    if (array1[i].Length <= 3)
-    {
-        NewArrayLength++;
-    }
-}
+    string[] array1 = new string[N];
+    FillArray(array1);
 
-// Формирование нового массива и вывод его на печать
-// либо сообщение об отсутствии данных для формирования нового массива
+    // Вычисление длины нового массива
 
-if (NewArrayLength > 0)
-{
-    string[] NewArray = new string[NewArrayLength];
-
-    int index = 0;
+    int NewArrayLength = 0;
     for (int i = 0; i < array1.Length; i++)
     {
         if (array1[i].Length <= 3)
         {
-            NewArray[index] = array1[i];
-            index++;
+            NewArrayLength++;
         }
     }
-    Console.WriteLine();
-    PrintArray(NewArray);
+
+    // Формирование нового массива и вывод его на печать
+    // либо сообщение об отсутствии данных для формирования нового массива
+
+    if (NewArrayLength > 0)
+    {
+        string[] NewArray = new string[NewArrayLength];
+
+        int index = 0;
+        for (int i = 0; i < array1.Length; i++)
+        {
+            if (array1[i].Length <= 3)
+            {
+                NewArray[index] = array1[i];
+                index++;
+            }
+        }
+        Console.WriteLine();
+        PrintArray(NewArray);
+    }
+    else
+    {
+        Console.WriteLine();
+        Console.Write("Все введенные строки имеют длину более 3 символов");
+    }
+
 }
 else
 {
-    Console.WriteLine();
-    Console.Write("Все введенные строки имеют длину более 3 символов");
+    Console.Write("Укажите число больше 0");
 }
+
+
